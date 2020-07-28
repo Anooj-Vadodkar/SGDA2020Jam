c424fd8cb825cd9c3346f4ef18ccbeab7c4c2ae0
Anooj Vadodkar <anoojrv@gmail.com>
2020-07-28 12:21:59 -0700

    Animations Added
    Added animations

--- SGDAJam/PlayerRelatedStuff/Player.gd
+++ SGDAJam/PlayerRelatedStuff/Player.gd
@@ -1,6 +1,8 @@
 extends KinematicBody2D
 
-
+const ACCELERATION = 1000
+const MAX_SPEED = 200
+const FRICTION = 1500
 var velocity = Vector2.ZERO
 
 
@@ -12,11 +14,12 @@ func _physics_process(delta):
 	var input_vector = Vector2.ZERO;
 	input_vector.x = (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"))
 	input_vector.y = (Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up"))
+	input_vector = input_vector.normalized();
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector * 3
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		velocity = Vector2.ZERO
-	move_and_collide(velocity)
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	velocity = move_and_slide(velocity)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
