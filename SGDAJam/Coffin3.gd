49655807d04ac3ec008d6f078854c5931764c19c
Anooj Vadodkar <anoojrv@gmail.com>
2020-07-30 19:50:27 -0700

    3rd Area

---
+++ SGDAJam/Coffin3.gd
@@ -0,0 +1,6 @@
+extends Area2D
+
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_RIGHT:
+		get_parent().knockback = Vector2.UP * 150
---
+++ SGDAJam/Coffin4Movement.gd
@@ -0,0 +1,8 @@
+extends KinematicBody2D
+
+
+var knockback = Vector2.ZERO
+# Called when the node enters the scene tree for the first time.
+func _physics_process(delta):
+	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
+	knockback = move_and_slide(knockback)
